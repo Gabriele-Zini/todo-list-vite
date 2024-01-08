b3c945ebb15929b575e6d4809144d1f44c0c24dc
Gabriele-Zini <139758244+Gabriele-Zini@users.noreply.github.com>
2024-01-08 11:30:09 +0100

    refactoring

--- server/logout.php
+++ server/logout.php
@@ -3,9 +3,7 @@ header("Access-Control-Allow-Origin: *");
 
 function logout() {
     session_start();
-    // Distruggi tutte le variabili di sessione
     session_unset();
-    // Distruggi la sessione
     session_destroy();
 
     echo json_encode(['success' => true, 'message' => 'Logout successo']);
